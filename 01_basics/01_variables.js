6912d18af353aa87ab80d5e92ceacf529dba89c3
Ansar Cheema <54665313+azcheema@users.noreply.github.com>
2024-08-24 08:39:19 +0000

    Variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId=144553
+let accountEmail="azc@google.com"
+var accountPassword="12345"
+accountCity="Tokyo"
+let accountState;
+// accountId=2; Value of constent cannot be changed
+accountEmail="cza@google.com"
+accountPassword="54321"
+accountCity="Malmo"
+
+console.log(accountId);
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
+
+console.log(typeof accountCity);
