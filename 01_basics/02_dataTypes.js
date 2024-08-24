75a3ca74911af0dd11e186364c2be306f4b55541
Ansar Cheema <54665313+azcheema@users.noreply.github.com>
2024-08-24 09:17:26 +0000

    DataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,22 @@
+"use strict" //treat all JS code as newer version
+
+// alert("hello"); We are using nodejs, not browser
+
+console.log(3+3)
+
+let name="Taha"
+let age=6
+let isLoggedIn=true
+
+// number => 2 to power 53
+// bigint = 10000
+// string = "Hi" OR single qoutes
+// boolean => true/false
+// null => standalone value
+// undefined => value is not assigned to the variable
+// symbol = unique
+
+// object
+
+console.log(typeof undefined); //undefined
+console.log(typeof null); // object
